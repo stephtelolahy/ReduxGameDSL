520adae44a85fe15b4bbe4bf46f3769e91b6f68b
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-04-02 06:24:45 +0200

    initial commit

---
+++ ReduxGameDSL/ReduxGameDSLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxGameDSLApp.swift
+//  ReduxGameDSL
+//
+//  Created by Hugues Telolahy on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReduxGameDSLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
