38e4632023ab6bcac5fc5c43f05925d1c61da2a9
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-04-08 00:40:51 +0200

    clean

--- Game/Sources/Game/Models/GameError.swift
+++ Game/Sources/Game/Models/GameError.swift
@@ -5,7 +5,7 @@
 //  Created by Hugues Telolahy on 07/04/2023.
 //
 
-/// Fatal game errors
+/// Game errors
 public enum GameError: Error, Codable, Equatable {
     case missingPlayer(String)
     case missingCard(String)
