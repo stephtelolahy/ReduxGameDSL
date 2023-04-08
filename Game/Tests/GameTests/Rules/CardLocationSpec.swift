ae908e333c0e19379bd4920f49092a6f84300c8e
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-04-08 14:37:48 +0200

    wip

--- Game/Tests/GameTests/Rules/CardLocationSpec.swift
+++ Game/Tests/GameTests/Rules/CardLocationSpec.swift
@@ -10,22 +10,22 @@ import Quick
 import Nimble
 
 final class CardLocationSpec: QuickSpec {
-    
+
     override func spec() {
         describe("adding") {
             context("location empty") {
                 it("should set first card") {
                     // Given
                     var sut = CardLocation()
-                    
+
                     // When
                     sut.add("c1")
-                    
+
                     // Then
                     expect(sut.cards) == ["c1"]
                 }
             }
-            
+
             context("location not empty") {
                 it("should append last card") {
                     // Given
@@ -33,32 +33,32 @@ final class CardLocationSpec: QuickSpec {
                         "c1"
                         "c2"
                     }
-                    
+
                     // When
                     sut.add("c3")
-                    
+
                     // Then
                     expect(sut.cards) == ["c1", "c2", "c3"]
                 }
             }
         }
-        
+
         describe("searching") {
-            context("all cards") {
+            context("an existing card") {
                 it("should be found") {
                     // Given
                     let sut = CardLocation {
                         "c1"
                         "c2"
                     }
-                    
+
                     // When
                     // Then
                     expect(sut.contains("c1")) == true
                     expect(sut.contains("c2")) == true
                 }
             }
-            
+
             context("missing card") {
                 it("should not be found") {
                     // Given
@@ -66,14 +66,14 @@ final class CardLocationSpec: QuickSpec {
                         "c1"
                         "c2"
                     }
-                    
+
                     // When
                     // Then
                     expect(sut.contains("c3")) == false
                 }
             }
         }
-        
+
         describe("removing") {
             context("card found") {
                 it("should remove") {
@@ -82,20 +82,20 @@ final class CardLocationSpec: QuickSpec {
                         "c1"
                         "c2"
                     }
-                    
+
                     // When
                     try sut.remove("c1")
-                    
+
                     // Then
                     expect(sut.cards) == ["c2"]
                 }
             }
-            
+
             context("card not found") {
                 it("should throw error") {
                     // Given
                     var sut = CardLocation()
-                    
+
                     // When
                     // Then
                     expect { try sut.remove("c1") }
