e21bdfabecb7bebe9f3949816402961be068e3e1
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-04-08 15:47:52 +0200

    wip

--- Game/Tests/GameTests/DSL/PlayerSpec.swift
+++ Game/Tests/GameTests/DSL/PlayerSpec.swift
@@ -12,13 +12,21 @@ final class PlayerSpec: QuickSpec {
     override func spec() {
         describe("a Player") {
             var sut: Player!
-            context("being initialized") {
+            context("default") {
                 beforeEach {
                     sut = Player()
                 }
 
                 it("should have no abilities") {
                     expect(sut.abilities).to(beEmpty())
+                }
+
+                it("should have hand limit = 0") {
+                    expect(sut.handLimit) == 0
+                }
+
+                it("should have empty hand") {
+                    expect(sut.hand.cards).to(beEmpty())
                 }
             }
 
@@ -35,6 +43,29 @@ final class PlayerSpec: QuickSpec {
 
                     // Then
                     expect(sut.abilities) == ["a1", "a2"]
+                }
+
+                it("should have hand limit") {
+                    // Given
+                    // When
+                    let sut = Player().handLimit(10)
+
+                    // Then
+                    expect(sut.handLimit) == 10
+                }
+
+                it("should have hand cards") {
+                    // Given
+                    // When
+                    let sut = Player {
+                        Hand {
+                            "c1"
+                            "c2"
+                        }
+                    }
+
+                    // Then
+                    expect(sut.hand.cards) == ["c1", "c2"]
                 }
             }
         }
