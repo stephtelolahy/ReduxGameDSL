3bcb289b014aa7470564a4159fdd48a71473e388
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-04-08 15:10:03 +0200

    wip

--- Game/Sources/Game/Models/GameError.swift
+++ Game/Sources/Game/Models/GameError.swift
@@ -7,7 +7,34 @@
 
 /// Game errors
 public enum GameError: Error, Codable, Equatable {
+
+    /// Expected a player with given identifier
     case missingPlayer(String)
+
+    /// Expected a card with given identifier
     case missingCard(String)
+
+    /// Expected cardStack not empty
     case stackIsEmpty
+
+    /// Expected players count to be leat X
+    case playersMustBeAtLeast(Int)
+
+    /// Expected player to be damaged
+    case playerAlreadyMaxHealth(String)
+
+    /// Expected player to have cards
+    case playerHasNoCard(String)
+
+    /// Expected player to have hand cards
+    case playerHasNoHandCard(String)
+
+    /// Expected player to have cards matching pattern
+    case playerHasNoMatchingCard(String)
+
+    /// Expected card to have onPlay effect
+    case cardHasNoPlayingEffect
+
+    /// Expected some player damaged
+    case noPlayerDamaged
 }
