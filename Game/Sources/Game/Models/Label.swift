044c20350121844d11d8edb4eeeb31d25af6bee8
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-04-16 07:26:59 +0200

    update

--- Game/Sources/Game/Models/Label.swift
+++ Game/Sources/Game/Models/Label.swift
@@ -7,10 +7,10 @@
 
 /// ChooseOne labels
 public enum Label {
+
     /// Random hand card label
-    /// displayed when choosing random hand card
     public static let randomHand = "randomHand"
 
-    /// Choose to pass when asked to do an action
+    /// Pass when asked to do an action
     public static let pass = "pass"
 }
