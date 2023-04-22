d68f9d17fd75cb4c5a595de44110f3040c878e25
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-04-22 21:51:51 +0200

    update

--- Modules/Tests/GameTests/Inventory/GatlingSpec.swift
+++ Modules/Tests/GameTests/Inventory/GatlingSpec.swift
@@ -72,8 +72,42 @@ final class GatlingSpec: QuickSpec {
             xcontext("two players") {
                 it("should allow each player to counter") {
                     // Given
+                    let state = GameState {
+                        Player("p1") {
+                            Hand {
+                                .gatling
+                            }
+                        }
+                        Player("p2") {
+                            Hand {
+                                .missed
+                            }
+                        }
+                    }
+                    let sut = createGameStore(initial: state)
+
                     // When
+                    var action = GameAction.play(actor: "p1", card: .gatling)
+                    var result = self.awaitAction(action, store: sut)
+
                     // Then
+                    let ctx2 = EffectContext(actor: "p1", card: .gatling, target: "p2")
+                    expect(result) == [.success(.play(actor: "p1", card: .gatling))]
+                    expect(sut.state.chooseOne) == ChooseOne(chooser: "p2", options: [
+                        .missed: .apply(.discard(player: .id("p2"), card: .id(.missed)), ctx: ctx2),
+                        Label.pass: .apply(.damage(1, player: .target), ctx: ctx2)
+                    ])
+
+                    // When p2 counter
+                    action = .apply(.discard(player: .id("p2"), card: .id(.missed)), ctx: ctx2)
+                    result = self.awaitAction(action, store: sut)
+
+                    // Then
+                    let ctx3 = EffectContext(actor: "p1", card: .gatling, target: "p3")
+                    expect(result) == [
+                        .success(.apply(.discard(player: .id("p2"), card: .id(.missed)), ctx: ctx2))
+                    ]
+                    expect(sut.state.chooseOne) == nil
                 }
             }
         }
