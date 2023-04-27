bb89b8265abd76b9c01583ef4cffcb735e4bacc6
Hugues Stephano Telolahy <stephano.telolahy@gmail.com>
2023-04-27 18:41:56 +0200

    Await bang

--- Modules/Tests/InventoryTests/Cards/DuelSpec.swift
+++ Modules/Tests/InventoryTests/Cards/DuelSpec.swift
@@ -13,12 +13,12 @@ import Inventory
 final class DuelSpec: QuickSpec {
     override func spec() {
         describe("playing Duel") {
-
+            
             xcontext("without target") {
                 it("should ask to select target") {
                 }
             }
-
+            
             context("with target") {
                 context("passing") {
                     it("should damage") {
@@ -32,30 +32,19 @@ final class DuelSpec: QuickSpec {
                             Player("p2")
                         }
                         let sut = createGameStore(initial: state)
-
+                        
                         // When
-                        var action = GameAction.play(actor: "p1", card: .duel, target: "p2")
-                        var result = self.awaitAction(action, store: sut)
-
-                        // Then
-                        expect(result) == [.success(.play(actor: "p1", card: .duel, target: "p2"))]
-                        let ctx2 = EffectContext(actor: "p1", card: .duel, target: "p2")
-                        expect(sut.state.chooseOne) == ChooseOne(chooser: "p2", options: [
-                            .pass: .damage(1, player: .target, ctx: ctx2)
-                        ])
-
-                        // When p2 passes
-                        action = .damage(1, player: .target, ctx: ctx2)
-                        result = self.awaitAction(action, store: sut)
-
+                        let action = GameAction.play(actor: "p1", card: .duel, target: "p2")
+                        let result = self.awaitAction(action, choices: [.pass], store: sut)
+                        
                         // Then
                         expect(result) == [
+                            .success(.play(actor: "p1", card: .duel, target: "p2")),
                             .success(.damage(1, player: .id("p2")))
                         ]
-                        expect(sut.state.chooseOne) == nil
                     }
                 }
-
+                
                 xcontext("discarding bang") {
                     it("should damage actor") {
                         // Given
@@ -63,7 +52,7 @@ final class DuelSpec: QuickSpec {
                         // Then
                     }
                 }
-
+                
                 xcontext("target and actor discarding bang") {
                     it("should damage target") {
                         // Given
--- Modules/Tests/InventoryTests/Misc/XCTestCase+Action.swift
+++ Modules/Tests/InventoryTests/Misc/XCTestCase+Action.swift
@@ -12,24 +12,35 @@ import Game
 extension XCTestCase {
     func awaitAction(
         _ action: GameAction,
+        choices: [String] = [],
         store: Store<GameState, GameAction>,
         timeout: TimeInterval = 0.1,
         file: StaticString = #file,
         line: UInt = #line
     ) -> [Result<GameAction, GameError>] {
 
+        var choices = choices
         var result: [Result<GameAction, GameError>] = []
-        let expectation = XCTestExpectation(description: "Awaiting publisher")
+        let expectation = XCTestExpectation(description: "Awaiting game idle")
         expectation.isInverted = true
         let cancellable = store.$state.sink { state in
             if let action = state.completedAction {
                 result.append(.success(action))
             }
+            
             if let error = state.thrownError {
                 result.append(.failure(error))
             }
+            
+            if let chooseOne = state.chooseOne,
+               !choices.isEmpty,
+               let choosenAction = chooseOne.options[choices.removeFirst()] {
+                DispatchQueue.main.async {
+                    store.dispatch(choosenAction)
+                }
+            }
 
-            if state.queue.isEmpty || state.chooseOne != nil {
+            if state.queue.isEmpty && state.chooseOne == nil {
                 expectation.fulfill()
             }
         }
