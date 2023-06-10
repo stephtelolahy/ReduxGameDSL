94ca6266812e3025da4f16e69163201057d66289
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-06-10 14:17:10 +0200

    Rename

--- Modules/Sources/Game/_Internal/Utils/EffectContext+Extensions.swift
+++ Modules/Sources/Game/_Internal/Utils/EffectContext+Extensions.swift
@@ -1,19 +1,19 @@
 //
-//  ContextKey+Extensions.swift
+//  EffectContext+Extensions.swift
 //  
 //
 //  Created by Hugues Telolahy on 20/05/2023.
 //
 
-extension Dictionary where Key == ContextKey, Value == String {
+extension EffectContext {
     func get(_ key: ContextKey) -> String {
         guard let value = self[key] else {
             fatalError("missing value for key \(key)")
         }
-        
+
         return value
     }
-    
+
     func copy(_ other: Self) -> Self {
         self.merging(other) { _, new in new }
     }
